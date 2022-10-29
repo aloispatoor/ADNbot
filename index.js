42c4777de6d1910d8d88c46c13fcd4e7fd4ea4e9
Alois Patoor <palois89@posteo.net>
2022-10-29 12:30:06 +0200

    functional bot

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Events, GatewayIntentBits, Collection } = require("discord.js");
+// const fs = require('node:fs');
+// const path = require('node:path');
+const { Client, Events, GatewayIntentBits, Collection, WelcomeChannel } = require("discord.js");
 const { token } = require('./config.json');
 
 // INITIALIZING CLIENT
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-client.commands = new Collection();
+const client = new Client({ 
+   intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMembers
+   ] });
 
-client.once('ready', () => {
-   console.log("Félicitations, votre bot Discord a été correctement initialisé !");
+client.on('ready', () => {
+   console.log("Bot Opérationnel");
 });
 
-// INITIALIZING COMMANDS
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
-};
-// SET ERROR MESSAGES
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-   const command = interaction.client.commands.get(interaction.commandName);
-   if(!command) {
-      console.error(`Il n'y a pas de commande du nom de ${interaction.commandName}.`);
+// MESSAGE
+client.on("messageCreate", message => {
+   if(message.author.bot){
       return;
    }
-
-   try {
-      await command.execute(interaction);
-   } catch (error) {
-      await interaction.reply({ content: 'Il y a eu une erreur en executant cette commande !', ephemeral: true});
+   console.log(message);
+   if(message.content === "Qui est l'antechrist ?"){
+      message.reply("<@" + "703541820201893930" + "> est l'antechrist selon Civitas");
+      // SEND A MESSAGE WITHOUT @ 
+      // message.channel.send("pong");
+   } else if(message.content === "help"){
+      message.reply("Mes commandes sont : \n Savoir 'Qui est l'antechrist ?' \n C'est tout, pour le moment.");
    }
+});
+
+// WELCOME MESSAGE FOR NEW MEMBERS
+client.on("guildMemberAdd", member => {
+   console.log("Un nouveau membre est arrivé !");
+   client.channels.cache.get("853360863569510404").send("<@" + member.id + ">, bienvenu-e chez nous. Installe-toi, on est bien :) \n Nous te demandons de lire notre channel <#" + "933145585144455208" + "> s'il te plaît. Merci ♥" );
 });
 
 client.login(token);
